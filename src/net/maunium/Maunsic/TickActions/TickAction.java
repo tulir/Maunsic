fe9d9308a8ec2aba05f4fe1109f5fb839bbdaede
Tulir293 <tulir293@maunium.net>
2015-06-12 16:36:16 +0300

    Add a simple system for actions that have to be executed each tick.

---
+++ src/net/maunium/Maunsic/TickActions/TickAction.java
@@ -0,0 +1,42 @@
+package net.maunium.Maunsic.TickActions;
+
+import net.minecraft.util.EnumChatFormatting;
+
+/**
+ * The base class for all Maunsic tick actions.
+ * 
+ * @author Tulir293
+ * @since 0.1
+ */
+public interface TickAction {
+	/**
+	 * @return The name of the action.
+	 */
+	public String getName();
+	
+	/**
+	 * @return True if this action is active and should be executed on the next tick. False otherwise.
+	 */
+	public boolean isActive();
+	
+	/**
+	 * Activate or deactivate this action.
+	 * 
+	 * @param active True if the action should be activated, false if it should be deactivated.
+	 */
+	public void setActive(boolean active);
+	
+	/**
+	 * @return The text to render on the left side of the screen when this action is active. An empty string will render an empty line. To render no text,
+	 *         return null.
+	 */
+	public default String getStatusText() {
+		// Default implementation returns the name and a green "ON" text
+		return getName() + EnumChatFormatting.GREEN + " ON";
+	}
+	
+	/**
+	 * Execute the action.
+	 */
+	public abstract void execute();
+}
---
+++ src/net/maunium/Maunsic/TickActions/TickActionHandler.java
@@ -0,0 +1,64 @@
+package net.maunium.Maunsic.TickActions;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+
+/**
+ * System to run specific tasks each tick if enabled.
+ * 
+ * @author Tulir293
+ * @since 0.1
+ */
+public class TickActionHandler {
+//	private Maunsic host;
+	private Set<TickAction> startActions = new HashSet<TickAction>(), endActions = new HashSet<TickAction>(), allActions = new HashSet<TickAction>();
+	
+//	public TickActionHandler(Maunsic host) {
+//		this.host = host;
+//	}
+	
+	/**
+	 * Register an action.
+	 * 
+	 * @param ta The action to register.
+	 * @param end True if the action should be called at the end of a tick event. False if it should be called at the start.
+	 */
+	public void registerAction(TickAction ta, boolean end) {
+		if (!end) startActions.add(ta);
+		else endActions.add(ta);
+		allActions.add(ta);
+	}
+	
+	/**
+	 * Unregister an action.
+	 * 
+	 * @param ta The action to unregister.
+	 * @param end The value used when registering the action.
+	 */
+	public void unregisterAction(TickAction ta, boolean end) {
+		if (!end) startActions.remove(ta);
+		else endActions.remove(ta);
+		allActions.remove(ta);
+	}
+	
+	@SubscribeEvent
+	public void onTick(TickEvent.PlayerTickEvent evt) {
+		if (evt.phase.equals(TickEvent.Phase.START)) {
+			for (TickAction ta : startActions)
+				if (ta.isActive()) ta.execute();
+		} else if (evt.phase.equals(TickEvent.Phase.END)) {
+			for (TickAction ta : endActions)
+				if (ta.isActive()) ta.execute();
+		}
+	}
+	
+	@SubscribeEvent
+	public void renderOverlay(RenderGameOverlayEvent.Text evt) {
+		for (TickAction ta : allActions)
+			if (ta.isActive()) evt.left.add(ta.getStatusText());
+	}
+}
