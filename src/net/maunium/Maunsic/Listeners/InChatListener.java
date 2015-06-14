749fcb96e3574eaf90e6bb21cffae4185de04ca2
Tulir293 <tulir293@maunium.net>
2015-06-14 17:05:13 +0300

    Added base64 en/decoding

--- src/net/maunium/Maunsic/Listeners/InChatListener.java
+++ src/net/maunium/Maunsic/Listeners/InChatListener.java
@@ -1,6 +1,15 @@
 package net.maunium.Maunsic.Listeners;
 
+import java.nio.charset.StandardCharsets;
+import java.util.regex.Pattern;
+
+import org.apache.commons.codec.binary.Base64;
+
 import net.maunium.Maunsic.Maunsic;
+
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.ChatStyle;
+import net.minecraft.util.EnumChatFormatting;
 
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
@@ -9,6 +18,15 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class InChatListener {
 	@SubscribeEvent(priority = EventPriority.HIGHEST)
 	public void onChat(ClientChatReceivedEvent evt) {
+		if (evt.message.getUnformattedText().contains("Ⅿᴮ")) {
+			String[] tmp = evt.message.getFormattedText().split(Pattern.quote("Ⅿᴮ"), 2);
+			tmp[1] = tmp[1].substring(0, tmp[1].length() - 2);
+			String decoded = new String(Base64.decodeBase64(tmp[1].getBytes(StandardCharsets.UTF_8)), StandardCharsets.UTF_8);
+			
+			evt.message = new ChatComponentText(tmp[0]);
+			evt.message.appendText(decoded.replace("&", "§"));
+			evt.message.appendSibling(new ChatComponentText(" [B64]").setChatStyle(new ChatStyle().setColor(EnumChatFormatting.GRAY).setItalic(true)));
+		}
 		Maunsic.getChatLogger().in(evt.message.getUnformattedText());
 	}
 }
--- src/net/maunium/Maunsic/Listeners/OutChatListener.java
+++ src/net/maunium/Maunsic/Listeners/OutChatListener.java
@@ -1,5 +1,10 @@
 package net.maunium.Maunsic.Listeners;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Locale;
+
+import org.apache.commons.codec.binary.Base64;
+
 import net.maunium.Maunsic.Maunsic;
 import net.maunium.Maunsic.Core.ClientChatSendEvent;
 
@@ -9,6 +14,19 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class OutChatListener {
 	@SubscribeEvent(priority = EventPriority.HIGH)
 	public void onSendChat(ClientChatSendEvent evt) {
+		String m = evt.getMessage().toLowerCase(Locale.ENGLISH);
+		if (m.startsWith("b64 ") || m.startsWith("base64 ")) {
+			String s = evt.getMessage().split(" ", 2)[1];
+			String encoded = new String(Base64.encodeBase64(s.getBytes(StandardCharsets.UTF_8)), StandardCharsets.UTF_8);
+			String msg = "Ⅿᴮ" + encoded;
+			if (msg.length() > 100) {
+				Maunsic.printChatError("message.encoding.toolong", msg.length());
+				evt.setCanceled(true);
+				return;
+			}
+			evt.setMessage(msg);
+			return;
+		}
 		Maunsic.getChatLogger().out(evt.getMessage());
 	}
 }
