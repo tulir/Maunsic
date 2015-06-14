21a883fa34ec41d500b465b36df01832e596f428
Tulir293 <tulir293@maunium.net>
2015-06-14 11:11:37 +0300

    Preliminary Config and Keybind GUI

---
+++ src/net/maunium/Maunsic/Gui/GuiKeybinds.java
@@ -0,0 +1,22 @@
+package net.maunium.Maunsic.Gui;
+
+import com.mcf.davidee.guilib.basic.BasicScreen;
+
+import net.minecraft.client.gui.GuiScreen;
+
+public class GuiKeybinds extends BasicScreen {
+	public GuiKeybinds(GuiScreen parent) {
+		super(parent);
+	}
+	
+	@Override
+	public void onInit() {
+		
+	}
+	
+	@Override
+	public void onRevalidate() {
+		
+	}
+	
+}
---
+++ src/net/maunium/Maunsic/Gui/GuiMaunsic.java
@@ -0,0 +1,59 @@
+package net.maunium.Maunsic.Gui;
+
+import com.mcf.davidee.guilib.basic.BasicScreen;
+import com.mcf.davidee.guilib.basic.Label;
+import com.mcf.davidee.guilib.core.Container;
+import com.mcf.davidee.guilib.vanilla.ButtonVanilla;
+
+import net.maunium.Maunsic.Maunsic;
+import net.maunium.Maunsic.Util.I18n;
+
+public class GuiMaunsic extends BasicScreen {
+	private Maunsic host;
+	
+	private Container[] pages;
+	private static int page = 0;
+	
+	private Label title;
+	private ButtonVanilla keys;
+	
+	public GuiMaunsic(Maunsic host) {
+		super(null);
+		this.host = host;
+	}
+	
+	@Override
+	protected void onInit() {
+		pages = new Container[] { new Container() };
+		
+		title = new Label("");
+		keys = new ButtonVanilla(I18n.translate("conf.keys"), this);
+		
+		pages[0].addWidgets(keys);
+	}
+	
+	@Override
+	protected void onRevalidate() {
+		containers.clear();
+		containers.add(pages[page]);
+		
+		title.setText(I18n.translate("conf.title", Maunsic.longVersion, page + 1));
+		title.setPosition(width / 2, 15);
+		pages[page].addWidgets(title);
+		
+		keys.setPosition(x(1), y(1));
+		
+		Maunsic.getLogger().trace("Revalidating container for page " + page, this);
+		pages[page].revalidate(0, 0, width, height);
+	}
+	
+	private int y(int y) {
+		return 25 + y * 25;
+	}
+	
+	private int x(int x) {
+		if (x == 1) return width / 2 - 150 - 12;
+		else if (x == 2) return width / 2 + 12;
+		else return x;
+	}
+}
