c10cedcec3e9597592f3bb0e8a3ce6ff954ee352
Tulir293 <tulir293@maunium.net>
2015-06-14 21:28:02 +0300

    Add Keybinding transformer

--- src/net/maunium/Maunsic/Core/Transformers/KeyBindingT.java
+++ src/net/maunium/Maunsic/Core/Transformers/KeyBindingT.java
@@ -1,5 +1,56 @@
 package net.maunium.Maunsic.Core.Transformers;
 
-public class KeyBindingT {
-	// TODO: Transform KeyBinding to send press and unpress calls to Maukeybinding.
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.InsnList;
+import org.objectweb.asm.tree.MethodInsnNode;
+import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.tree.VarInsnNode;
+
+import net.maunium.Maunsic.Core.AbstractMauTransformer;
+import net.maunium.Maunsic.Util.MauKeybinding;
+
+public class KeyBindingT extends AbstractMauTransformer {
+	public KeyBindingT() {
+		super("net.minecraft.client.settings.KeyBinding", "");
+	}
+	
+	@Override
+	public byte[] transform(byte[] bytes, boolean obf) {
+		String methodName1 = obf ? "" : "onTick", methodName2 = obf ? "" : "setKeyBindState";
+		
+		ClassNode node = new ClassNode();
+		ClassReader cr = new ClassReader(bytes);
+		cr.accept(node, 0);
+		
+		int a = 0;
+		for (MethodNode m : node.methods) {
+			if (m.name.equals(methodName1) && m.desc.equals("(I)V")) {
+				InsnList insn = new InsnList();
+				
+				insn.add(new VarInsnNode(Opcodes.ILOAD, 0));
+				insn.add(new MethodInsnNode(Opcodes.INVOKESTATIC, MauKeybinding.class.getName().replace('.', '/'), "onTick", "(I)V", false));
+				
+				m.instructions.insert(insn);
+				a++;
+				if (a == 2) break;
+			} else if (m.name.equals(methodName2) && m.desc.equals("(IZ)V")) {
+				InsnList insn = new InsnList();
+				
+				insn.add(new VarInsnNode(Opcodes.ILOAD, 0));
+				insn.add(new VarInsnNode(Opcodes.ILOAD, 1));
+				insn.add(new MethodInsnNode(Opcodes.INVOKESTATIC, MauKeybinding.class.getName().replace('.', '/'), "onTick", "(I)V", false));
+				
+				m.instructions.insert(insn);
+				a++;
+				if (a == 2) break;
+			}
+		}
+		
+		ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_MAXS | ClassWriter.COMPUTE_FRAMES);
+		node.accept(cw);
+		return cw.toByteArray();
+	}
 }
