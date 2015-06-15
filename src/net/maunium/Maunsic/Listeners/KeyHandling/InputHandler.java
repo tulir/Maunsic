dac2fa68c793e4ebe9a215798a8330af01e8e54f
Tulir293 <tulir293@maunium.net>
2015-06-15 13:09:08 +0300

    Add konami code unlocking and nofall keybind

--- src/net/maunium/Maunsic/Listeners/KeyHandling/InputHandler.java
+++ src/net/maunium/Maunsic/Listeners/KeyHandling/InputHandler.java
@@ -21,17 +21,23 @@ import net.minecraftforge.fml.client.FMLClientHandler;
  */
 public class InputHandler {
 	private static boolean disabled = false;
+	private static final int[] konami = { Keyboard.KEY_UP, Keyboard.KEY_UP, Keyboard.KEY_DOWN, Keyboard.KEY_DOWN, Keyboard.KEY_LEFT, Keyboard.KEY_RIGHT,
+			Keyboard.KEY_LEFT, Keyboard.KEY_RIGHT, Keyboard.KEY_B, Keyboard.KEY_A };
+	private static int konamiStatus = 0;
+	private static boolean konamiDown = true;
 	private static Maunsic host;
 	
 	public static void setHost(Maunsic newHost) {
 		host = newHost;
 		KeyRegistry.registerKeybind(Keybinds.config);
 		KeyRegistry.registerKeybind(Keybinds.fly);
+		KeyRegistry.registerKeybind(Keybinds.nofall);
 		KeyRegistry.registerKeybind(Keybinds.inc_speed);
 		KeyRegistry.registerKeybind(Keybinds.dec_speed);
 	}
 	
 	public static void input(int keycode, boolean pressed) {
+		if (keycode == -101) return;
 		// Check if usage is allowed
 		if (!ServerHandler.canUse()) return;
 		// Execute the precheck key maucros, but only if the input handler is enabled.
@@ -51,31 +57,41 @@ public class InputHandler {
 			if (km.getExecutionPhase().equals(ep)) km.checkAndExecute();
 		}
 		
-		// Check for the input handler toggle combination.
-		if (Keyboard.isKeyDown(Keyboard.KEY_M) && Keyboard.isKeyDown(Keyboard.KEY_A) && Keyboard.isKeyDown(Keyboard.KEY_U)) {
+		if (keycode == konami[konamiStatus] && pressed == konamiDown) {
+			if (!konamiDown) konamiStatus++;
+			konamiDown = !konamiDown;
+		} else {
+			konamiStatus = 0;
+			konamiDown = true;
+		}
+		
+		if (konamiStatus == 10) {
 			disabled = !disabled;
+			konamiStatus = 0;
+			konamiDown = true;
 			Maunsic.printChat("conf.keys." + (disabled ? "disable" : "enable"));
 			return;
 		}
 		
-		// Check if the input handler is disabled.
-		if (disabled) return;
-		
-		if (keycode == Keybinds.config.getKeyCode()) Minecraft.getMinecraft().displayGuiScreen(new GuiMaunsic(host));
-		else if (keycode == Keybinds.fly.getKeyCode()) {
-			if (!host.actionFly.isActive()) {
-				if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT)) host.actionFly.setType(ActionFly.TYPE_WALK);
-				else host.actionFly.setType(ActionFly.TYPE_FLY);
-			} else host.actionFly.setType(ActionFly.TYPE_DISABLED);
-		} else if (keycode == Keybinds.inc_speed.getKeyCode()) {
-			if (Keybinds.dec_speed.isDown()) host.actionFly.setSpeed(ActionFly.DEFAULT_SPEED);
-			else host.actionFly.changeSpeed(true);
-		} else if (keycode == Keybinds.inc_speed.getKeyCode()) {
-			if (Keybinds.inc_speed.isDown()) host.actionFly.setSpeed(ActionFly.DEFAULT_SPEED);
-			else host.actionFly.changeSpeed(false);
+		if (pressed && !disabled) {
+			if (keycode == Keybinds.config.getKeyCode()) Minecraft.getMinecraft().displayGuiScreen(new GuiMaunsic(host));
+			else if (keycode == Keybinds.fly.getKeyCode()) {
+				if (!host.actionFly.isActive()) {
+					if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT)) host.actionFly.setType(ActionFly.TYPE_WALK);
+					else host.actionFly.setType(ActionFly.TYPE_FLY);
+				} else host.actionFly.setType(ActionFly.TYPE_DISABLED);
+			} else if (keycode == Keybinds.inc_speed.getKeyCode()) {
+				if (Keybinds.dec_speed.isDown()) host.actionFly.setSpeed(ActionFly.DEFAULT_SPEED);
+				else host.actionFly.changeSpeed(true);
+			} else if (keycode == Keybinds.dec_speed.getKeyCode()) {
+				if (Keybinds.inc_speed.isDown()) host.actionFly.setSpeed(ActionFly.DEFAULT_SPEED);
+				else host.actionFly.changeSpeed(false);
+			} else if (keycode == Keybinds.nofall.getKeyCode()) {
+				host.actionNofall.setActive(!host.actionNofall.isActive());
+			}
 		}
 		
-		for (KeyMaucro km : KeyMaucro.getKeyMaucros()) {
+		if (!disabled) for (KeyMaucro km : KeyMaucro.getKeyMaucros()) {
 			KeyMaucro.ExecPhase ep = pressed ? KeyMaucro.ExecPhase.POSTKEYS_DOWN : KeyMaucro.ExecPhase.POSTKEYS_UP;
 			if (km.getExecutionPhase().equals(ep)) km.checkAndExecute();
 		}
@@ -93,6 +109,7 @@ public class InputHandler {
 	public static class Keybinds {
 		public static final MauKeybind config = new MauKeybind("Configuration", Keyboard.KEY_F4);
 		public static final MauKeybind fly = new MauKeybind("Fly", Keyboard.KEY_F);
+		public static final MauKeybind nofall = new MauKeybind("nofall", Keyboard.KEY_N);
 		public static final MauKeybind inc_speed = new MauKeybind("Increase Speed", Keyboard.KEY_PRIOR);
 		public static final MauKeybind dec_speed = new MauKeybind("Decrease Speed", Keyboard.KEY_NEXT);
 	}
