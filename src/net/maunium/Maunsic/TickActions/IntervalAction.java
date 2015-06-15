eede65bc4564ccff6b2a81733e240b9446cbec6d
Tulir293 <tulir293@maunium.net>
2015-06-15 21:34:40 +0300

    Add the base for interval actions

---
+++ src/net/maunium/Maunsic/TickActions/IntervalAction.java
@@ -0,0 +1,26 @@
+package net.maunium.Maunsic.TickActions;
+
+import net.minecraft.client.Minecraft;
+
+public abstract class IntervalAction implements TickAction {
+	protected int interval;
+	protected long previousExec = 0;
+	
+	public IntervalAction() {
+		this(10);
+	}
+	
+	public IntervalAction(int interval) {
+		this.interval = interval;
+	}
+	
+	public abstract void executeInterval();
+	
+	@Override
+	public void execute() {
+		if (Minecraft.getSystemTime() - previousExec > interval) {
+			executeInterval();
+			previousExec = Minecraft.getSystemTime();
+		}
+	}
+}
