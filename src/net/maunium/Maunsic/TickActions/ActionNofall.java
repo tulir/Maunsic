2fa5805429c2e2678ab9814a0fbcffdda005433f
Tulir293 <tulir293@maunium.net>
2015-06-15 13:08:42 +0300

    Add action nofall

---
+++ src/net/maunium/Maunsic/TickActions/ActionNofall.java
@@ -0,0 +1,27 @@
+package net.maunium.Maunsic.TickActions;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.play.client.C03PacketPlayer;
+
+public class ActionNofall implements TickAction {
+	private boolean active = false;
+	
+	@Override
+	public String getName() {
+		return "Nofall";
+	}
+	
+	@Override
+	public boolean isActive() {
+		return active;
+	}
+	
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+	
+	@Override
+	public void execute() {
+		Minecraft.getMinecraft().thePlayer.sendQueue.addToSendQueue(new C03PacketPlayer(true));
+	}
+}
