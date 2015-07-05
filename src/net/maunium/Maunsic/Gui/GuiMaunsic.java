833f304faeadf2b9d91f00cd2129b25af2f1dd16
Tulir293 <tulir293@maunium.net>
2015-07-05 16:36:35 +0300

    Add a dynamic window size-based paging system.

--- src/net/maunium/Maunsic/Gui/GuiMaunsic.java
+++ src/net/maunium/Maunsic/Gui/GuiMaunsic.java
@@ -1,12 +1,20 @@
 package net.maunium.Maunsic.Gui;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.mcf.davidee.guilib.basic.BasicScreen;
 import com.mcf.davidee.guilib.basic.Label;
 import com.mcf.davidee.guilib.core.Button;
 import com.mcf.davidee.guilib.core.Container;
+import com.mcf.davidee.guilib.core.Widget;
 import com.mcf.davidee.guilib.vanilla.ButtonVanilla;
+import com.mcf.davidee.guilib.vanilla.extended.ExtendedIntSlider;
+import com.mcf.davidee.guilib.vanilla.extended.StateButton;
 
 import net.maunium.Maunsic.Maunsic;
+import net.maunium.Maunsic.Actions.ActionBlink;
+import net.maunium.Maunsic.Actions.ActionFly;
 import net.maunium.Maunsic.Gui.Alts.GuiChangeUsername;
 import net.maunium.Maunsic.Gui.KeyMaucros.GuiKeyMaucros;
 import net.maunium.Maunsic.Gui.KeyMaucros.GuiLuaThreads;
@@ -24,11 +32,15 @@ import net.minecraft.client.Minecraft;
 public class GuiMaunsic extends BasicScreen {
 	private Maunsic host;
 	
-	private Container[] pages;
 	private static int page = 0;
+	private int pageCount = 1;
+	private Container c;
+	private List<Widget> widgets = new ArrayList<Widget>();
 	
 	private Label title;
-	private ButtonVanilla keys, alts, keymaucros, luathreads, xray;
+	private ButtonVanilla keys, alts, keymaucros, luathreads, xray, fanda, prevPage, nextPage, close;
+	private StateButton autophase, autoblink, autophasesprint;
+	private ExtendedIntSlider speed, jump, blinksafety;
 	
 	public GuiMaunsic(Maunsic host) {
 		super(null);
@@ -37,61 +49,105 @@ public class GuiMaunsic extends BasicScreen {
 	
 	@Override
 	protected void onInit() {
-		pages = new Container[] { new Container() };
-		
+		c = new Container();
+		containers.add(c);
 		title = new Label("");
+		
+		prevPage = new ButtonVanilla(I18n.translate("conf.prevpage"), this);
+		nextPage = new ButtonVanilla(I18n.translate("conf.nextpage"), this);
+		close = new ButtonVanilla(I18n.translate("conf.backtogame"), this);
+		
 		alts = new ButtonVanilla(I18n.translate("conf.alts"), this);
 		keymaucros = new ButtonVanilla(I18n.translate("conf.km"), this);
 		luathreads = new ButtonVanilla(I18n.translate("conf.lua.threads"), this);
 		keys = new ButtonVanilla(I18n.translate("conf.keys"), this);
 		xray = new ButtonVanilla(I18n.translate("conf.xray"), this);
+		fanda = new ButtonVanilla(I18n.translate("conf.fanda"), this);
+		add(widgets, alts, keymaucros, luathreads, keys, xray, fanda);
 		
-		pages[0].addWidgets(keys, alts, keymaucros, luathreads, xray);
+		speed = new ExtendedIntSlider(I18n.translate("conf.amv.speed"), host.actionFly.getSpeed(), ActionFly.MIN_SPEED, ActionFly.MAX_SPEED);
+		jump = new ExtendedIntSlider(amount -> amount == 0 ? I18n.translate("conf.amv.jump.default") : I18n.translate("conf.amv.jump", amount),
+				host.actionFly.getJump(), 0, 100);
+		add(widgets, speed, jump);
+		
+		autoblink = new StateButton(150, 20, I18n.translate("conf.blink.automated"), this, ActionBlink.automated ? 1 : 0);
+		blinksafety = new ExtendedIntSlider(amount -> amount == 0 ? I18n.translate("conf.blink.safety") + ": " + I18n.translate("off")
+				: I18n.translate("conf.blink.safety") + ": " + amount + " " + I18n.translate("conf.blink.safety.level"), ActionBlink.safetyLevel, 0, 10);
+		autophase = new StateButton(150, 20, I18n.translate("conf.phase.automated"), this, host.actionPhase.automated ? 1 : 0);
+		autophasesprint = new StateButton(150, 20, I18n.translate("conf.phase.autoforward"), this, host.actionPhase.autoforward ? 1 : 0);
+		add(widgets, autoblink, blinksafety, autophase, autophasesprint);
+	}
+	
+	public void add(List<Widget> l, Widget... w) {
+		for (Widget ww : w)
+			l.add(ww);
 	}
 	
 	@Override
 	protected void onRevalidate() {
-		containers.clear();
-		containers.add(pages[page]);
+		c.getWidgets().clear();
 		
 		title.setText(I18n.translate("conf.title", Maunsic.longVersion, page + 1));
 		title.setPosition(width / 2, 15);
-		pages[page].addWidgets(title);
+		c.addWidgets(title);
 		
-		keys.setPosition(x(1), y(1));
-		alts.setPosition(x(2), y(1));
-		keymaucros.setPosition(x(1), y(2));
-		luathreads.setPosition(x(2), y(2));
-		xray.setPosition(x(1), y(3));
+		int x1 = width / 2 - 150 - 12, x2 = width / 2 + 12;
+		int pagesize = 2 * ((height - 90) / 25), y = 2, start = page * pagesize, end = start + pagesize;
+		pageCount = (int) Math.ceil((double) widgets.size() / (double) pagesize);
+		if (end > widgets.size()) end = widgets.size();
+		boolean side = true;
+		
+		for (int i = start; i < end; i++) {
+			Widget w = widgets.get(i);
+			w.setPosition(side ? x1 : x2, y * 25);
+			side = !side;
+			if (side) y++;
+			c.addWidgets(w);
+		}
+		
+		prevPage.setPosition(x1, height - 40);
+		nextPage.setPosition(x2, height - 40);
+		if (page < 1) {
+			close.setPosition(x1, height - 40);
+			c.addWidgets(close);
+		} else if (page > pageCount - 2) {
+			close.setPosition(x2, height - 40);
+			c.addWidgets(close);
+		}
+		
+		if (page < pageCount - 1) c.addWidgets(nextPage);
+		if (page > 0) c.addWidgets(prevPage);
 		
 		Maunsic.getLogger().trace("Revalidating container for page " + page, this);
-		pages[page].revalidate(0, 0, width, height);
+		c.revalidate(0, 0, width, height);
 	}
 	
 	@Override
 	protected void onButtonClicked(Button b, int code) {
 		if (code != 0) return;
 		// Yes, they are supposed to be the same instance, so using == is just fine.
-		if (b == keys) Minecraft.getMinecraft().displayGuiScreen(new GuiKeybinds(this, host));
+		if (b == close) close();
+		else if (b == nextPage) {
+			page++;
+			onRevalidate();
+		} else if (b == prevPage) {
+			page--;
+			onRevalidate();
+		} else if (b == keys) Minecraft.getMinecraft().displayGuiScreen(new GuiKeybinds(this, host));
 		else if (b == alts) Minecraft.getMinecraft().displayGuiScreen(new GuiChangeUsername(host));
 		else if (b == keymaucros) Minecraft.getMinecraft().displayGuiScreen(new GuiKeyMaucros(this, host));
 		else if (b == luathreads) Minecraft.getMinecraft().displayGuiScreen(new GuiLuaThreads(this));
 		else if (b == xray) Minecraft.getMinecraft().displayGuiScreen(new GuiXrayBlocks(host));
+		else if (b == fanda) Minecraft.getMinecraft().displayGuiScreen(new GuiFriendsAndAttacking(this, host));
 	}
 	
 	@Override
 	protected void save() {
+		host.actionFly.setSpeed(speed.getIntValue());
+		host.actionFly.setJump(jump.getIntValue());
+		// Save all the action configs to RAM
 		host.getActionHandler().saveAll();
+		// Save config to disk.
 		host.saveConfig();
-	}
-	
-	private int y(int y) {
-		return 25 + y * 25;
-	}
-	
-	private int x(int x) {
-		if (x == 1) return width / 2 - 150 - 12;
-		else if (x == 2) return width / 2 + 12;
-		else return x;
 	}
 }
