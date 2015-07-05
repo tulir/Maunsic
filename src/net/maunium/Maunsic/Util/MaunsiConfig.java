1d86d4212f96096fe71c4168fea3b4e27056d586
Tulir293 <tulir293@maunium.net>
2015-07-05 12:29:34 +0300

    Added collection setting and list getting. Changed set methods to wrap the object in a JsonPrimitive and only use the JsonElement recursive set method.

--- src/net/maunium/Maunsic/Util/MaunsiConfig.java
+++ src/net/maunium/Maunsic/Util/MaunsiConfig.java
@@ -5,13 +5,18 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.regex.Pattern;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonIOException;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSyntaxException;
 import com.google.gson.stream.JsonWriter;
 
@@ -213,7 +218,7 @@ public class MaunsiConfig {
 	/**
 	 * Get the value in the path as a String. If the value is not a String, return the given value.
 	 * 
-	 * @param path The path to String.
+	 * @param path The path to search.
 	 * @param def The default value to return if there is no valid String in the given path.
 	 * @return The String in the given path, or the given default value if there was no valid String in the given path.
 	 */
@@ -229,13 +234,74 @@ public class MaunsiConfig {
 	}
 	
 	/**
+	 * Get the value in the path as a List. If the value is not a List, return null.
+	 * 
+	 * @param path The path to search.
+	 * @return The List in the given path, or null if there was no valid List in the given path.
+	 */
+	public List<JsonElement> getList(String path) {
+		return getList(path, null);
+	}
+	
+	/**
+	 * Get the value in the path as a List. If the value is not a List, return the given value.
+	 * 
+	 * @param path The path to search.
+	 * @param def The default value to return if there is no valid List in the given path.
+	 * @return The List in the given path, or the given default value if there was no valid List in the given path.
+	 */
+	public List<JsonElement> getList(String path, List<JsonElement> def) {
+		JsonElement e = get(json, path);
+		if (e == null) return def;
+		if (!e.isJsonArray()) return def;
+		List<JsonElement> l = new ArrayList<JsonElement>();
+		JsonArray ja = e.getAsJsonArray();
+		for (JsonElement je : ja)
+			l.add(je);
+		return l;
+	}
+	
+	/**
+	 * Get the value in the path as a List. If the value is not a List, return null.
+	 * 
+	 * @param path The path to search.
+	 * @return The List in the given path, or null if there was no valid List in the given path.
+	 */
+	public List<String> getStringList(String path) {
+		return getStringList(path, null);
+	}
+	
+	/**
+	 * Get the value in the path as a List of Strings. If the value is not a List, return the given value.
+	 * 
+	 * @param path The path to search.
+	 * @param def The default value to return if there is no valid List in the given path.
+	 * @return The List in the given path, or the given default value if there was no valid List in the given path.
+	 */
+	public List<String> getStringList(String path, List<String> def) {
+		JsonElement e = get(json, path);
+		if (e == null) return def;
+		if (!e.isJsonArray()) return def;
+		List<String> l = new ArrayList<String>();
+		JsonArray ja = e.getAsJsonArray();
+		for (JsonElement je : ja) {
+			try {
+				l.add(je.getAsString());
+			} catch (ClassCastException | IllegalStateException ex) {
+				return def;
+			}
+		}
+		return l;
+	}
+	
+	/**
 	 * Get the value in the path as a JsonElement. If the value is not a JsonElement, return null.
 	 * 
 	 * @param path The path to search.
 	 * @return The JsonElement in the given path, or null if there was no valid JsonElement in the given path.
 	 */
 	public JsonElement get(String path) {
-		return get(path, null);
+		return get(path, new JsonObject());
 	}
 	
 	/**
@@ -273,8 +339,7 @@ public class MaunsiConfig {
 	 * @param obj The String.
 	 */
 	public void set(String path, String obj) {
-		if (path.indexOf('.') == -1) json.addProperty(path, obj);
-		else set(json, path, obj);
+		set(json, path, new JsonPrimitive(obj));
 	}
 	
 	/**
@@ -284,7 +349,7 @@ public class MaunsiConfig {
 	 * @param obj The Number.
 	 */
 	public void set(String path, Number obj) {
-		set(json, path, obj);
+		set(json, path, new JsonPrimitive(obj));
 	}
 	
 	/**
@@ -294,7 +359,7 @@ public class MaunsiConfig {
 	 * @param obj The Boolean.
 	 */
 	public void set(String path, Boolean obj) {
-		set(json, path, obj);
+		set(json, path, new JsonPrimitive(obj));
 	}
 	
 	/**
@@ -304,7 +369,7 @@ public class MaunsiConfig {
 	 * @param obj The Character.
 	 */
 	public void set(String path, Character obj) {
-		set(json, path, obj);
+		set(json, path, new JsonPrimitive(obj));
 	}
 	
 	/**
@@ -318,71 +383,21 @@ public class MaunsiConfig {
 	}
 	
 	/**
-	 * Hey! Don't you touch this! It's mine! But seriously, don't use this. It's only supposed to be used by the public methods for recursive abilities.
+	 * Set the value in the given path to the given collection.
+	 * 
+	 * @param path The path.
+	 * @param obj The collection.
 	 */
-	private void set(JsonObject jo, String path, String obj) {
-		if (path.indexOf('.') == -1) jo.addProperty(path, obj);
-		else {
-			String[] ss = path.split(Pattern.quote("."), 2);
-			JsonObject jo2;
-			JsonElement je = jo.get(ss[0]);
-			if (je == null) {
-				jo2 = new JsonObject();
-				jo.add(ss[0], jo2);
-			} else jo2 = je.getAsJsonObject();
-			set(jo2, ss[1], obj);
+	public void set(String path, Collection<Object> obj) {
+		JsonArray ja = new JsonArray();
+		for (Object o : obj) {
+			if (o instanceof String) ja.add(new JsonPrimitive((String) o));
+			else if (o instanceof Number) ja.add(new JsonPrimitive((Number) o));
+			else if (o instanceof Boolean) ja.add(new JsonPrimitive((Boolean) o));
+			else if (o instanceof Character) ja.add(new JsonPrimitive((Character) o));
+			else if (o instanceof JsonElement) ja.add((JsonElement) o);
 		}
-	}
-	
-	/**
-	 * Hey! Don't you touch this! It's mine! But seriously, don't use this. It's only supposed to be used by the public methods for recursive abilities.
-	 */
-	private void set(JsonObject jo, String path, Number obj) {
-		if (path.indexOf('.') == -1) jo.addProperty(path, obj);
-		else {
-			String[] ss = path.split(Pattern.quote("."), 2);
-			JsonObject jo2;
-			JsonElement je = jo.get(ss[0]);
-			if (je == null) {
-				jo2 = new JsonObject();
-				jo.add(ss[0], jo2);
-			} else jo2 = je.getAsJsonObject();
-			set(jo2, ss[1], obj);
-		}
-	}
-	
-	/**
-	 * Hey! Don't you touch this! It's mine! But seriously, don't use this. It's only supposed to be used by the public methods for recursive abilities.
-	 */
-	private void set(JsonObject jo, String path, Boolean obj) {
-		if (path.indexOf('.') == -1) jo.addProperty(path, obj);
-		else {
-			String[] ss = path.split(Pattern.quote("."), 2);
-			JsonObject jo2;
-			JsonElement je = jo.get(ss[0]);
-			if (je == null) {
-				jo2 = new JsonObject();
-				jo.add(ss[0], jo2);
-			} else jo2 = je.getAsJsonObject();
-			set(jo2, ss[1], obj);
-		}
-	}
-	
-	/**
-	 * Hey! Don't you touch this! It's mine! But seriously, don't use this. It's only supposed to be used by the public methods for recursive abilities.
-	 */
-	private void set(JsonObject jo, String path, Character obj) {
-		if (path.indexOf('.') == -1) jo.addProperty(path, obj);
-		else {
-			String[] ss = path.split(Pattern.quote("."), 2);
-			JsonObject jo2;
-			JsonElement je = jo.get(ss[0]);
-			if (je == null) {
-				jo2 = new JsonObject();
-				jo.add(ss[0], jo2);
-			} else jo2 = je.getAsJsonObject();
-			set(jo2, ss[1], obj);
-		}
+		set(json, path, ja);
 	}
 	
 	/**
