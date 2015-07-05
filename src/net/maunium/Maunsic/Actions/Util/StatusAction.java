4c5c4a9edfc75afd8c6d6e359be16a7eceea3e91
Tulir293 <tulir293@maunium.net>
2015-07-05 22:49:44 +0300

    Move getStatusText to the end

--- src/net/maunium/Maunsic/Actions/Util/StatusAction.java
+++ src/net/maunium/Maunsic/Actions/Util/StatusAction.java
@@ -9,11 +9,6 @@ import net.maunium.Maunsic.Util.MaunsiConfig;
  * @since 0.1
  */
 public interface StatusAction {
-	/**
-	 * @return The text to render on the left side of the screen when this action is active. An empty string will render an empty line and a null string will be
-	 *         ignored. To render no text, return null.
-	 */
-	public String[] getStatusText();
 	
 	/**
 	 * @return True if this action is active and should be executed on the next tick. False otherwise.
@@ -30,4 +25,10 @@ public interface StatusAction {
 	public void saveData(MaunsiConfig conf);
 	
 	public void loadData(MaunsiConfig conf);
+	
+	/**
+	 * @return The text to render on the left side of the screen when this action is active. An empty string will render an empty line and a null string will be
+	 *         ignored. To render no text, return null.
+	 */
+	public String[] getStatusText();
 }
