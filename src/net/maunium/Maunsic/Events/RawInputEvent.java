824a7fdf1249a81d16dcbe2eb81fbc6ea41b7f0e
Tulir293 <tulir293@maunium.net>
2015-07-12 20:40:44 +0300

    Remove the test code

--- src/net/maunium/Maunsic/Events/RawInputEvent.java
+++ src/net/maunium/Maunsic/Events/RawInputEvent.java
@@ -1,18 +1,10 @@
 package net.maunium.Maunsic.Events;
 
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.eventhandler.Cancelable;
 import net.minecraftforge.fml.common.eventhandler.Event;
 
 @Cancelable
 public class RawInputEvent extends Event {
-	public void d(int keycode, boolean pressed) {
-		RawInputEvent rie = new RawInputEvent(keycode, pressed);
-		if (MinecraftForge.EVENT_BUS.post(rie)) return;
-		keycode = rie.getCode();
-		pressed = rie.isPressed();
-	}
-	
 	private int code;
 	private boolean pressed;
 	
