547d32d7701bc01844c53603e27e197c2d3ee0b7
Tulir293 <tulir293@maunium.net>
2015-07-12 20:40:32 +0300

    Fix memory stack ids

--- src/net/maunium/Maunsic/Core/Transformers/KeyBindingT.java
+++ src/net/maunium/Maunsic/Core/Transformers/KeyBindingT.java
@@ -51,15 +51,15 @@ public class KeyBindingT extends AbstractMauTransformer {
 				
 				insn.add(new TypeInsnNode(Opcodes.NEW, RawInputEvent.class.getName().replace('.', '/')));
 				insn.add(new InsnNode(Opcodes.DUP));
+				insn.add(new VarInsnNode(Opcodes.ILOAD, 0));
 				insn.add(new VarInsnNode(Opcodes.ILOAD, 1));
-				insn.add(new VarInsnNode(Opcodes.ILOAD, 2));
 				insn.add(new MethodInsnNode(Opcodes.INVOKESPECIAL, RawInputEvent.class.getName().replace('.', '/'), "<init>", "(IZ)V", false));
-				insn.add(new VarInsnNode(Opcodes.ASTORE, 3));
+				insn.add(new VarInsnNode(Opcodes.ASTORE, 2));
 				LabelNode l1 = new LabelNode();
 				insn.add(l1);
 				insn.add(new FieldInsnNode(Opcodes.GETSTATIC, MinecraftForge.class.getName().replace('.', '/'), "EVENT_BUS",
 						"L" + EventBus.class.getName().replace('.', '/') + ";"));
-				insn.add(new VarInsnNode(Opcodes.ALOAD, 3));
+				insn.add(new VarInsnNode(Opcodes.ALOAD, 2));
 				insn.add(new MethodInsnNode(Opcodes.INVOKEVIRTUAL, EventBus.class.getName().replace('.', '/'), "post",
 						"(L" + Event.class.getName().replace('.', '/') + ";)Z", false));
 				LabelNode l2 = new LabelNode();
@@ -67,14 +67,14 @@ public class KeyBindingT extends AbstractMauTransformer {
 				insn.add(new InsnNode(Opcodes.RETURN));
 				insn.add(l2);
 				insn.add(new FrameNode(Opcodes.F_APPEND, 1, new Object[] { RawInputEvent.class.getName().replace('.', '/') }, 0, null));
-				insn.add(new VarInsnNode(Opcodes.ALOAD, 3));
+				insn.add(new VarInsnNode(Opcodes.ALOAD, 2));
 				insn.add(new MethodInsnNode(Opcodes.INVOKEVIRTUAL, RawInputEvent.class.getName().replace('.', '/'), "getCode", "()I", false));
-				insn.add(new VarInsnNode(Opcodes.ISTORE, 1));
+				insn.add(new VarInsnNode(Opcodes.ISTORE, 0));
 				LabelNode l3 = new LabelNode();
 				insn.add(l3);
-				insn.add(new VarInsnNode(Opcodes.ALOAD, 3));
+				insn.add(new VarInsnNode(Opcodes.ALOAD, 2));
 				insn.add(new MethodInsnNode(Opcodes.INVOKEVIRTUAL, RawInputEvent.class.getName().replace('.', '/'), "isPressed", "()Z", false));
-				insn.add(new VarInsnNode(Opcodes.ISTORE, 2));
+				insn.add(new VarInsnNode(Opcodes.ISTORE, 1));
 				
 				m.instructions.insert(insn);
 				break;
