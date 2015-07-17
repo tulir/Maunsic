ab1b6dff34d564a5cd3594bde5f680f1b9ee0c5c
Tulir293 <tulir293@maunium.net>
2015-07-17 15:33:32 +0300

    Fairly well working language GUI

--- src/net/maunium/Maunsic/Gui/GuiLanguage.java
+++ src/net/maunium/Maunsic/Gui/GuiLanguage.java
@@ -1,14 +1,21 @@
 package net.maunium.Maunsic.Gui;
 
+import java.io.File;
+import java.util.regex.Pattern;
+
 import com.mcf.davidee.guilib.basic.BasicScreen;
+import com.mcf.davidee.guilib.basic.FocusedContainer;
 import com.mcf.davidee.guilib.basic.Label;
 import com.mcf.davidee.guilib.core.Button;
 import com.mcf.davidee.guilib.core.Container;
 import com.mcf.davidee.guilib.core.Scrollbar;
 import com.mcf.davidee.guilib.vanilla.ButtonVanilla;
+import com.mcf.davidee.guilib.vanilla.ScrollbarVanilla;
 
 import net.maunium.Maunsic.Maunsic;
 import net.maunium.Maunsic.Util.I18n;
+
+import net.minecraft.client.gui.Gui;
 
 /**
  * Gui for language channging
@@ -30,62 +37,59 @@ public class GuiLanguage extends BasicScreen {
 	
 	@Override
 	protected void onInit() {
-		c = new Container();
+		s = new ScrollbarVanilla(10);
 		title = new Label(I18n.translate("conf.language.title"));
+		refresh = new ButtonVanilla(I18n.translate("conf.language.refresh"), this);
+		back = new ButtonVanilla(I18n.translate("conf.back"), this);
 		
-//		automated = new StateButton(I18n.translate("conf.phase.automated"), host.actionPhase.automated ? 1 : 0);
-//		noclip = new StateButton(I18n.translate("conf.phase.noclip"), host.actionPhase.noclip ? 1 : 0);
-//		ground = new StateButton(I18n.translate("conf.phase.ground"), host.actionPhase.ground ? 1 : 0);
-//		fall = new StateButton(I18n.translate("conf.phase.fall"), host.actionPhase.fall ? 1 : 0);
-//		
-//		verticalPackets = new ExtendedIntSlider(I18n.translate("conf.phase.packets.vertical"), host.actionPhase.verticalPackets, 1, 10);
-//		horizontalPackets = new ExtendedIntSlider(I18n.translate("conf.phase.packets.horizontal"), host.actionPhase.horizontalPackets, 1, 10);
-//		movVertical = new ExtendedIntSlider(new ExtendedIntSlider.Format() {
-//			@Override
-//			public String format(int amount) {
-//				return I18n.translate("conf.phase.move.vertical") + ": " + amount / 10.0;
-//			}
-//		}, (int) (host.actionPhase.movVertical * 10), 1, 50);
-//		movHorizontal = new ExtendedIntSlider(new ExtendedIntSlider.Format() {
-//			@Override
-//			public String format(int amount) {
-//				return I18n.translate("conf.phase.move.horizontal") + ": " + amount / 10.0;
-//			}
-//		}, (int) (host.actionPhase.movHorizontal * 10), 1, 50);
-		
-		back = new ButtonVanilla(200, 20, I18n.translate("conf.back"), this);
-		
-		c.addWidgets(title, back);
-		containers.add(lc);
-		containers.add(c);
+		c = new Container();
+		c.addWidgets(title, back, refresh);
 	}
 	
 	@Override
 	protected void onRevalidate() {
+		containers.clear();
 		title.setPosition(width / 2, 15);
 		
-//		automated.setPosition(width / 2 - automated.getWidth() - 12, 40);
-//		noclip.setPosition(width / 2 - noclip.getWidth() - 12, 65);
-//		ground.setPosition(width / 2 - ground.getWidth() - 12, 90);
-//		fall.setPosition(width / 2 - fall.getWidth() - 12, 115);
-//		
-//		verticalPackets.setPosition(width / 2 + 12, 40);
-//		horizontalPackets.setPosition(width / 2 + 12, 65);
-//		movVertical.setPosition(width / 2 + 12, 90);
-//		movHorizontal.setPosition(width / 2 + 12, 115);
+		lc = new FocusedContainer(s, 14, 8);
 		
-		back.setPosition(width / 2 - back.getWidth() / 2, height - 40);
+		int i = 0;
+		String ss = host.getConfig().getString("language", "en_US");
+		for (File f : Maunsic.getConfDir("language").listFiles()) {
+			ButtonVanilla sb = new ButtonVanilla(200, 20, f.getName().split(Pattern.quote("."), 2)[0], this);
+			if (sb.getText().equals(ss)) sb.setText("[Active] " + sb.getText());
+			sb.setPosition(width / 2 - sb.getWidth() / 2, 40 + i * 25);
+			lc.addWidgets(sb);
+			
+			i++;
+		}
 		
+		s.setPosition(width / 2 + 170, 0);
+		back.setPosition(width / 2 - back.getWidth() - 12, height - 40);
+		refresh.setPosition(width / 2 + 12, height - 40);
+		
+		containers.add(lc);
+		containers.add(c);
 		c.revalidate(0, 0, width, height);
+		lc.revalidate(0, 35, width, height - 83);
 	}
 	
 	@Override
-	public void save() {
-	
+	public void drawBackground() {
+		super.drawBackground();
+		Gui.drawRect(lc.left(), lc.top(), lc.right(), lc.bottom(), 0x44444444);
 	}
 	
 	@Override
 	public void onButtonClicked(Button b, int code) {
-		close();
+		System.out.println(b.getText());
+		if (b == back) close();
+		else if (b == refresh) onRevalidate();
+		else {
+			Maunsic.getLogger().info("Changing language to " + b.getText());
+			host.getConfig().set("language", b.getText());
+			I18n.reinitMaunsicI18n(host, true);
+			onRevalidate();
+		}
 	}
 }
